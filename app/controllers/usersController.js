14a32aef9a203614989d2818dc7166c333c25e96
Ahmadreza Enayati <ahmadre147@gmail.com>
2024-02-02 12:03:35 +0330

    add user login and sign up controllers

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -2,10 +2,87 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken'); 
 const config = require('../config/auth');
 
-module.exports.register = async (req, res) => {  
-  // Create user logic  
+module.exports.register = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+    
+    const { name, email, password } = req.body;
+    
+    try {
+        // Check if user exists
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ msg: 'User already exists' });  
+        }
+
+        // Insert user
+        user = new User({
+            name, 
+            email,
+            password 
+        });
+
+        // Encrypt password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.json({ msg: 'Registration successful' });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
 };
 
 module.exports.login = async (req, res) => {
-  // Validate and return JWT 
-};
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ 
+            errors: errors.array() 
+        });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        // Check for existing user
+        const user = await User.findOne({ email });
+
+        if (!user) {
+        return res.status(400).json({
+            message: 'User does not exist'
+        });
+        }
+
+        // Validate password  
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+        return res.status(400).json({
+            message: 'Incorrect password'
+        });
+        }
+
+        // Sign JWT token
+        const payload = { 
+            user: {
+                id: user.id  
+            }
+        };
+
+        jwt.sign(payload, jwtSecret, {expiresIn: 0}, 
+        (err, token) => {
+            if(err) throw err;
+            res.json({token});
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error'); 
+    }
+  };
--- app/routes/api.js
+++ app/routes/api.js
@@ -4,9 +4,29 @@ const router = express.Router();
 const usersCtrl = require('../controllers/usersController.js');
 const postsCtrl = require('../controllers/postsController');
 
+const { check, validationResult } = require('express-validator');
+
 // Users routes  
-router.post('/users', usersCtrl.register);
-router.post('/users/login', usersCtrl.login);
+router.post('/users', [
+                check('name')
+                    .exists()
+                    .withMessage('Name is required'),
+                check('email')
+                    .isEmail() 
+                    .withMessage('Invalid email'),
+                check('password')
+                    .isLength({ min: 6})
+                    .withMessage('Password must be at least 6 characters long'),
+            ], usersCtrl.register);
+
+router.post('/users/login', [
+                check('email')
+                .isEmail()
+                .withMessage('Please enter a valid email'),
+                check('password')
+                .exists()
+                .withMessage('Password is required')
+            ], usersCtrl.login);
 
 // Posts routes
 router.get('/posts', postsCtrl.getPosts); 
